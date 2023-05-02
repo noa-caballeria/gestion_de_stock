d0411c230867110a3687c52bc67afa28e3bc4dcd
noa-caballeria <127933518+noa-caballeria@users.noreply.github.com>
2023-05-02 14:53:38 +0200

    Update ma_boutique.py

--- boutique/ma_boutique.py
+++ boutique/ma_boutique.py
@@ -4,7 +4,7 @@ import mysql.connector
 db = mysql.connector.connect(
     host = "localhost",
     user = "root",
-    password = "NoaLilly13600@",
+    password = "",
     database = "boutique"
 )
 
